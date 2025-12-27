b2ba1e8c0bef4d8dcde0fbc03d623e3441b71c58
FrengkiSimatupang17 <frengkihotsabarsimatupang1@gmail.com>
2025-12-27 15:46:07 +0700

    Fix: Force HTTPS on production to solve mixed content

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -4,6 +4,7 @@ namespace App\Providers;
 
 use Illuminate\Support\ServiceProvider;
 use Illuminate\Support\Facades\Notification;
+use Illuminate\Support\Facades\URL;
 use App\Channels\WhatsAppChannel;
 
 class AppServiceProvider extends ServiceProvider
@@ -25,7 +26,12 @@ class AppServiceProvider extends ServiceProvider
         // Logika pemaksa HTTPS untuk Ngrok sudah DIHAPUS.
         // Sekarang aplikasi berjalan normal di mode HTTP (Localhost).
 
-        // 1. Registrasi Custom Notification Channel (WhatsApp)
+        if($this->app->environment('production')) {
+            URL::forceScheme('https');
+        }
+
+        // 2. Registrasi Custom Notification Channel (WhatsApp)
+        // (Kode asli Anda tetap aman di sini)
         Notification::extend('whatsapp', function ($app) {
             return new WhatsAppChannel();
         });
